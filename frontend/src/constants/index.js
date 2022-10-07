8c7d5e8492be7bcfc156a02e1ceddf26188e6ef2
ThanhNT26 <ngtruongthanh98@gmail.com>
2022-10-07 19:22:17 +0700

    fetch: add category page

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,82 @@
+export const categoryList = [
+  {
+    image: "https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn",
+    title: "Men Clothes",
+  },
+  {
+    image: "https://cf.shopee.vn/file/31234a27876fb89cd522d7e3db1ba5ca_tn",
+    title: "Mobile & Gadgets",
+  },
+  {
+    image: "https://cf.shopee.vn/file/978b9e4cb61c611aaaf58664fae133c5_tn",
+    title: "Consumer Electronics",
+  },
+  {
+    image: "https://cf.shopee.vn/file/c3f3edfaa9f6dafc4825b77d8449999d_tn",
+    title: "Computer & Accessories",
+  },
+  {
+    image: "https://cf.shopee.vn/file/ec14dd4fc238e676e43be2a911414d4d_tn",
+    title: "Cameras",
+  },
+  {
+    image: "https://cf.shopee.vn/file/86c294aae72ca1db5f541790f7796260_tn",
+    title: "Watches",
+  },
+  {
+    image: "https://cf.shopee.vn/file/74ca517e1fa74dc4d974e5d03c3139de_tn",
+    title: "Men Shoes",
+  },
+  {
+    image: "https://cf.shopee.vn/file/7abfbfee3c4844652b4a8245e473d857_tn",
+    title: "Home Appliances",
+  },
+  {
+    image: "https://cf.shopee.vn/file/6cb7e633f8b63757463b676bd19a50e4_tn",
+    title: "Sport & Outdoor",
+  },
+  {
+    image: "https://cf.shopee.vn/file/3fb459e3449905545701b418e8220334_tn",
+    title: "Automotive",
+  },
+  {
+    image: "https://cf.shopee.vn/file/75ea42f9eca124e9cb3cde744c060e4d_tn",
+    title: "Women Clothes",
+  },
+  {
+    image: "https://cf.shopee.vn/file/099edde1ab31df35bc255912bab54a5e_tn",
+    title: "Moms, Kids & Babies",
+  },
+  {
+    image: "https://cf.shopee.vn/file/24b194a695ea59d384768b7b471d563f_tn",
+    title: "Home & Living",
+  },
+  {
+    image: "https://cf.shopee.vn/file/ef1f336ecc6f97b790d5aae9916dcb72_tn",
+    title: "Beauty",
+  },
+  {
+    image: "https://cf.shopee.vn/file/49119e891a44fa135f5f6f5fd4cfc747_tn",
+    title: "Health",
+  },
+  {
+    image: "https://cf.shopee.vn/file/48630b7c76a7b62bc070c9e227097847_tn",
+    title: "Women Shoes",
+  },
+  {
+    image: "https://cf.shopee.vn/file/fa6ada2555e8e51f369718bbc92ccc52_tn",
+    title: "Women Bags",
+  },
+  {
+    image: "https://cf.shopee.vn/file/8e71245b9659ea72c1b4e737be5cf42e_tn",
+    title: "Fashion Accessories",
+  },
+  {
+    image: "https://cf.shopee.vn/file/c432168ee788f903f1ea024487f2c889_tn",
+    title: "Grocery",
+  },
+  {
+    image: "https://cf.shopee.vn/file/36013311815c55d303b0e6c62d6a8139_tn",
+    title: "Books & Stationery",
+  },
+];
--- frontend/src/views/products/ProductPage.vue
+++ frontend/src/views/products/ProductPage.vue
@@ -1,17 +1,55 @@
 <template>
   <div class="product-page">
     <header-component />
-    <h1>Products</h1>
+    <h1>Category</h1>
+
+    <div class="product-page-body">
+      <div class="category-container">
+        <!-- <div class="row">
+          <div class="col">
+            <div class="category-item">
+              <img
+                src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn"
+                alt=""
+                class="image"
+              />
+              <div class="title">Men Clothes</div>
+            </div>
+
+            <div class="category-item">
+              <img
+                src="https://cf.shopee.vn/file/687f3967b7c2fe6a134a2c11894eea4b_tn"
+                alt=""
+                class="image"
+              />
+              <div class="title">Men Clothes</div>
+            </div>
+          </div>
+        </div> -->
+
+        <div
+          v-for="(categoryItem, index) in categoryList"
+          :key="index"
+          class="category-item"
+        >
+          <img :src="categoryItem.image" alt="" class="image" />
+          <div class="title">{{ categoryItem.title }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import HeaderComponent from "@/components/Header/HeaderComponent";
+import { categoryList } from "@/constants";
 
 export default {
   name: "ProductPage",
   data: function () {
-    return {};
+    return {
+      categoryList,
+    };
   },
   components: {
     HeaderComponent,
@@ -21,6 +59,33 @@ export default {
 
 <style lang="scss" scoped>
 .product-page {
-  color: $color-primary;
+  &-body {
+    margin: 0px 200px 0px 200px;
+
+    background-color: azure;
+
+    .category-container {
+      display: grid;
+      grid-auto-flow: column dense; /* column flow with "dense" to fill all the cells */
+      grid-template-rows: 150px 150px; /* 2 rows */
+      grid-auto-columns: 100px;
+      justify-content: center;
+
+      .category-item {
+        transition: opacity 0.2s ease;
+        border-right: 1px solid rgba(0, 0, 0, 0.05);
+        border-bottom: 1px solid rgba(0, 0, 0, 0.05);
+        width: 100px;
+
+        .image {
+          width: 100px;
+          height: 100px;
+        }
+
+        .title {
+        }
+      }
+    }
+  }
 }
 </style>
