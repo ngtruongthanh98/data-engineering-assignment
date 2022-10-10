cf95eb40b348f6e7463b12a6fab8d68fb66482ec
ThanhNT26 <ngtruongthanh98@gmail.com>
2022-10-10 20:22:30 +0700

    fetch: setup chartjs

--- frontend/src/views/home/HomePage.vue
+++ frontend/src/views/home/HomePage.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="home">
+  <div class="home-page">
     <header-component />
+
+    <div class="home-page-body">
+      <bar-chart class="bar-chart-1" />
+    </div>
   </div>
 </template>
 
 <script>
 import HeaderComponent from "@/components/Header/HeaderComponent";
+import BarChart from "@/components/BarChart";
 
 export default {
   name: "HomePage",
@@ -14,12 +19,18 @@ export default {
   },
   components: {
     HeaderComponent,
+    BarChart,
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.home {
-  color: $color-primary;
+.home-page {
+  &-body {
+    .bar-chart-1 {
+      width: 300px;
+      height: 300px;
+    }
+  }
 }
 </style>
