75837a1bfea166c9a7da28cc7fc327d7c4ebe831
Vincent1411 <vuthanhnhan1411@gmail.com>
2022-11-01 22:52:41 +0700

    feature: comment pagination

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,12 @@ app.config.from_object(__name__)
 CORS(app, resources={r"/*": {'origins': "*"}})
 
 # hello world route
+
+
 @app.route('/', methods=['GET'])
 def greeting():
   return 'Hello World'
+
 
 dynamodb = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
 
@@ -22,7 +25,7 @@ product_index = ['ProductName', 'SubcategoryName', 'CategoryName']
 def get_products():
   query_columns = request.args.to_dict(flat=False)
   columns = [qc for qc in query_columns]
-  
+
   if any([c for c in columns if c in product_index]):
     if query_columns.get('ProductName') is not None:
       return dynamodb.get_item(TableName="Product", Key={
@@ -52,6 +55,30 @@ def get_products():
   #     print("🚀 ~ file: main.py ~ line 56 ~ expression_attribute_values", expression_attribute_values)
   #   return dynamodb.scan(
   #       TableName="Product", FilterExpression="ProductName = :z", ExpressionAttributeValues={':z': {'S':'HL Nipple'}})
-  
+
+
+@app.route('/comments', methods=['GET'])
+def get_comments():
+  max_items = request.args.get('max_items')
+  max_items = 5 if max_items is not None else 5
+  page_size = request.args.get('page_size')
+  page_size = 5 if page_size is not None else 5
+  starting_token = request.args.get('starting_token')
+  productID = request.args.get('productID')
+
+  if productID is None: return {"status": False, "message": "provide productID"}
+
+  paginator = dynamodb.get_paginator('query')
+  result = paginator.paginate(TableName="Comment",
+                                       ExpressionAttributeValues={':productID': {
+                                           'N': productID,
+                                       }}, KeyConditionExpression='ProductID = :productID',
+                                       PaginationConfig={
+                                           'MaxItems': max_items,
+                                           'PageSize': page_size,
+                                           'StartingToken': starting_token
+                                       }).build_full_result()
+  return result
+
 if __name__ == '__main__':
   app.run(debug=True)
